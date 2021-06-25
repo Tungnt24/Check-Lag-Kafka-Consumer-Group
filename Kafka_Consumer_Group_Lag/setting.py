82d7131691821b11bddeda8582a1300bd1a146ff
tungnguyentu <tungnguyentu@bizflycloud.vn>
2021-06-25 18:30:09 +0700

    update config

--- Kafka_Consumer_Group_Lag/setting.py
+++ Kafka_Consumer_Group_Lag/setting.py
@@ -7,6 +7,6 @@ load_dotenv()
 class TeleBotConfig:
     token = os.getenv("TELEBOT_TOKEN")
     api = os.getenv("TELE_API")
-    chat_ids = os.getenv("CHAT_ID")
+    chat_id = os.getenv("CHAT_ID")
     kowl_api = os.getenv("KOWL_API")
     consumer_group = os.getenv("CONSUMER_GROUP").split(",")
