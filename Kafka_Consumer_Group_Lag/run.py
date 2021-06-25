67589449fa17ee8ff4566315bf18f1fbd3fb81a4
tungnguyentu <tungnguyentu@bizflycloud.vn>
2021-06-25 18:28:13 +0700

    update logic

--- Kafka_Consumer_Group_Lag/run.py
+++ Kafka_Consumer_Group_Lag/run.py
@@ -1,13 +1,20 @@
-from Kafka_Consumer_Group_Lag.telegram_bot.lagbot import telebot, get_data_kafka_group, get_lag
+from Kafka_Consumer_Group_Lag.telegram_bot.lagbot import (
+    telebot,
+    get_data_kafka_group,
+    get_lag,
+)
 import schedule
 import time
 
+
 def job():
-	data = get_data_kafka_group()
-	consumer_group_lag = get_lag(data)
-	telebot(consumer_group_lag)
+    data = get_data_kafka_group()
+    consumer_group_lag = get_lag(data)
+    telebot(consumer_group_lag)
+
+
 if __name__ == "__main__":
-	schedule.every(10).seconds.do(job)
-	while True:
-		schedule.run_pending()
-		time.sleep(1)
+    schedule.every(30).seconds.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
--- Kafka_Consumer_Group_Lag/telegram_bot/lagbot.py
+++ Kafka_Consumer_Group_Lag/telegram_bot/lagbot.py
@@ -1,29 +1,39 @@
 from Kafka_Consumer_Group_Lag.setting import TeleBotConfig
+from Kafka_Consumer_Group_Lag.utils.logger import logger
 import requests
 import json
 
 
 def get_data_kafka_group():
-	res = requests.get(TeleBotConfig.kowl_api)
-	if res.status_code != 200:
-		return
-	return res.content
+    res = requests.get(TeleBotConfig.kowl_api)
+    if res.status_code != 200:
+        logger.error(f"requests to kowl api | status code {res.status_code}")
+        return
+    return res.content
 
 
 def get_lag(data):
-	result = []
-	groups_data = json.loads(data)
-	for key, value in groups_data.items():
-		for group in value:
-			if group['groupId'] in TeleBotConfig.consumer_group:
-				for topic in group['lag']['topicLags']:
-					if topic['summedLag'] >= 100:
-						result.append(f"{group['groupId']} | total lag: {topic['summedLag']}")
-	return result
+    result = []
+    groups_data = json.loads(data)
+    for key, value in groups_data.items():
+        for group in value:
+            if group["groupId"] in TeleBotConfig.consumer_group:
+                for topic in group["lag"]["topicLags"]:
+                    if topic["summedLag"] >= 100:
+                        result.append(
+                            f"{group['groupId']} | total lag: {topic['summedLag']}"
+                        )
+    return result
 
 
 def telebot(consumer_group_lag):
-	api = TeleBotConfig.api
-	text_format = '\n'.join(consumer_group_lag)
-	for chat_id in TeleBotConfig.chat_ids:
-		requests.post(api.format(TeleBotConfig.token, chat_id, f"``` {text_format} ```"))
+    api = TeleBotConfig.api
+    text_format = "\n".join(consumer_group_lag)
+    logger.info(f"SENDING MESSAGE: {text_format}")
+    requests.post(
+        api.format(
+            TeleBotConfig.token,
+            TeleBotConfig.chat_id,
+            f"``` {text_format} ```",
+        )
+    )
